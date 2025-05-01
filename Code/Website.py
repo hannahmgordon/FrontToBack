94c063f3c88239f8e10a563b19200eea7f5478ec
hannahmgordon <hmg226@lehigh.edu>
2025-04-30 23:54:25 -0400

    some report

--- Code/Website.py
+++ Code/Website.py
@@ -89,7 +89,7 @@ elif page == "Annual 10-K Returns by Company":
         
         st.pyplot(fig)
 
-        st.markdown("### Line Color Legend")
+        st.markdown("### Bin Color Legend")
         sp500_note = "<span style='color:#bbbbbb; font-weight:bold;'>■</span> S&P 500 Annual Return (faint grey line)"
         st.markdown(f"<div style='font-size:16px; margin-bottom:0.5em'>{sp500_note}</div>", unsafe_allow_html=True)
 
@@ -132,4 +132,32 @@ elif page == "Cosine Similarity vs Monthly Return Over Time":
 # ---------- Page: Report ----------
 else:
     st.title("Report")
-    st.write("Coming soon.")
+    st.write("""
+10-K filings are rich sources of data about a firm's financial health, strategy, and risk factors. However, due to their length and complexity, investors may not fully process all the nuanced information they contain. The "Lazy Prices" paper, which this code closely aligns with, posits that changes in the language used in these filings can signal important shifts in a company's prospects. The authors demonstrate that these textual changes have predictive power for future stock returns, suggesting that the market underreacts to this information initially.
+
+To empirically investigate this theory, this repo is designed to contain code that performs textual analysis tasks on the 10-K files from 1993 to 2024 for the S&P 500 firms.
+
+These tasks are performed within the file named TextualAnalysis.ipynb:
+
+- Data Acquisition and Loading: The first step is to gather all the 10-K filings to analyse. There's three ways to go about this:
+
+    - One could fetch the documents using the SEC EDGAR downloader. This method provides the most direct access to the original data but requires significant amount of time to process, clean and prepare the text for analysis.
+    - One can also download a giant 50 GB folder of all 10-X files in a zipped format. The raw text files are cleaned by removing non-textual data, structural markup, and irrelevant headers/footers to isolate the textual content for analysis.
+    - Alternatively, one can also use the Loughran-McDonald 10-K Document Dictionaries file. This 15.8GB (unzipped) text file contains word counts for each word in the LM dictionary for every 10-K filing in the dataset. This was the quickest option, and we decided to use it. 
+
+- Now that the data is prepared, for the actual textual analysis, we determined it would be best to utilise the cosine similarity to measure the changes in the texts.
+
+    - Our code extracts word counts from the 10-K dictionary for each S&P 500 firm. These counts are converted into numerical vectors, with each dimension representing a word's frequency.
+    - Cosine similarity is calculated to compare word count vectors between consecutive years. Cosine distance (1 - cosine similarity) quantifies the year-over-year change in textual content.
+
+- Next, the cosine distances are merged with financial data (stock returns) for S&P 500 firms. This merge connects textual changes to how the market values the firms.
+- Filing dates are incorporated to ensure accurate timing of information and returns.
+
+- The data was sorted into five bins each year based on Cosine Similarity, ranking firms from those with the least change in their 10-K filings (Bin 1) to those with the most change (Bin 5).
+
+Our final data set contains the following attributes (columns):
+
+| Symbol      | CIK | Filing Date | Filing Year  | Cosine Distance | Cosine Similarity | Return Measures | Bin |
+
+The team has created some graphs and visuals in the Visualization.ipynb file to gather some interesting findings from the work.
+""")
